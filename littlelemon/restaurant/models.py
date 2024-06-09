71d61ea0cb5f7f222abaa8706f8d9c103ed98bbc
Jpvenegas21 <161311460+Jpvenegas21@users.noreply.github.com>
2024-06-08 23:15:13 -0400

    Fourth change

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_guest = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)])
+    booking_date = models.DateTimeField()
+class Menu(models.Model):
+    tittle = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
